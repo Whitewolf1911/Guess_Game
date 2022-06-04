a5ba14038d9d401a26839d291d8add133826ce86
Whitewolf1911 <alibasoglu1911@gmail.com>
2022-06-04 15:07:40 +0300

    Final. Added timer, end of the game screen , play again functionality.

--- app/src/main/java/com/example/guessgameapp/GameFragment.kt
+++ app/src/main/java/com/example/guessgameapp/GameFragment.kt
@@ -1,11 +1,13 @@
 package com.example.guessgameapp
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.example.guessgameapp.databinding.ActivityMainBinding
 import com.example.guessgameapp.databinding.FragmentGameBinding
@@ -16,16 +18,31 @@ class GameFragment : Fragment() {
     private var score: Int = 0
     var word = ""
     private lateinit var wordList: MutableList<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val countDownTimer = object : CountDownTimer(60000, 1000) {
+            // override object functions here, do it quicker by setting cursor on object, then type alt + enter ; implement members
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvCountdown.text =  (millisUntilFinished / 1000).toString()
+            }
+
+            override fun onFinish() {
+                gameFinished()
+            }
+        }
         score = 0
+        countDownTimer.start()
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
         binding.apply {
@@ -90,7 +107,12 @@ class GameFragment : Fragment() {
 
     private fun gameFinished() {
 
-        findNavController().navigate(R.id.action_gameFragment_to_scoreFragment)
+        val args : Bundle =  Bundle()
+        args.putString("END_SCORE", score.toString())
+        view?.post{
+            findNavController().navigate(R.id.action_gameFragment_to_scoreFragment, args)
+        }
+        fragmentManager?.fragments?.remove(this)
     }
 
     private fun updateScoreText() {
--- app/src/main/java/com/example/guessgameapp/ScoreFragment.kt
+++ app/src/main/java/com/example/guessgameapp/ScoreFragment.kt
@@ -5,20 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.guessgameapp.databinding.FragmentScoreBinding
 
 
 class ScoreFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentScoreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_score, container, false)
+        binding.tvEndScore.text = arguments?.getString("END_SCORE")
+        binding.btnPlayAgain.setOnClickListener {
+            findNavController().navigate(R.id.action_scoreFragment_to_titleFragment)
+        }
+        return binding.root
     }
 }
